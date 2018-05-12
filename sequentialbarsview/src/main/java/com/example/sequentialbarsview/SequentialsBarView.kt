b1bf3204dcd1bc546a8d5d6f9e9aa90e0956af14
Anwesh43 <anweshthecool0@gmail.com>
2018-05-13 00:25:39 +0530

    adding SequentialsBarView class

--- sequentialbarsview/src/main/java/com/example/sequentialbarsview/SequentialsBarView.kt
+++ sequentialbarsview/src/main/java/com/example/sequentialbarsview/SequentialsBarView.kt
@@ -3,3 +3,27 @@ package com.example.sequentialbarsview
 /**
  * Created by anweshmishra on 13/05/18.
  */
+
+import android.graphics.*
+import android.content.*
+import android.view.View
+import android.view.MotionEvent
+
+class SequentialBarsView (ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+
+}
