30daf032adb26299642a3a920844a2544dc53f33
Anwesh43 <anweshthecool0@gmail.com>
2018-05-13 01:14:08 +0530

    adding Activity in View

--- app/src/main/java/com/example/anweshmishra/kotlinsequentialbarsview/MainActivity.kt
+++ app/src/main/java/com/example/anweshmishra/kotlinsequentialbarsview/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.anweshmishra.kotlinsequentialbarsview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.sequentialbarsview.SequentialsBarView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        SequentialsBarView.create(this)
     }
 }
