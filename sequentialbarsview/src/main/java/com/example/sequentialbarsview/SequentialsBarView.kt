b3e5b01a59cf889193326d2213f62cc9f1476d36
Anwesh43 <anweshthecool0@gmail.com>
2018-05-13 00:30:09 +0530

    adding State class

--- sequentialbarsview/src/main/java/com/example/sequentialbarsview/SequentialsBarView.kt
+++ sequentialbarsview/src/main/java/com/example/sequentialbarsview/SequentialsBarView.kt
@@ -9,7 +9,7 @@ import android.content.*
 import android.view.View
 import android.view.MotionEvent
 
-class SequentialBarsView (ctx : Context) : View(ctx) {
+class SequentialsBarView (ctx : Context) : View(ctx) {
 
     private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
 
@@ -26,4 +26,30 @@ class SequentialBarsView (ctx : Context) : View(ctx) {
         return true
     }
 
+    data class State(var prevScale : Float = 0f, var dir : Float = 0f, var j : Int = 0) {
+
+        private val scales : Array<Float> = arrayOf(0f, 0f, 0f, 0f, 0f)
+
+        fun update(stopcb : (Float) -> Unit) {
+            scales[j] += 0.1f * dir
+            if (Math.abs(scales[j] - prevScale) > 1) {
+                prevScale = scales[j]
+                j += dir.toInt()
+                if (j == scales.size || j == -1) {
+                    j -= dir.toInt()
+                    dir = 0f
+                    prevScale = scales[j]
+                    stopcb(prevScale)
+                }
+            }
+        }
+
+        fun startUpdating(startcb : () -> Unit) {
+            if (dir == 0f) {
+                dir = 1 - 2 * prevScale
+                startcb()
+            }
+        }
+
+    }
 }
