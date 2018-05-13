a04a1979241a29b5a862b5878f54b9f293d0b579
Anwesh43 <anweshthecool0@gmail.com>
2018-05-13 14:56:39 +0530

    adding OnBarMoveListener in activity

--- app/src/main/java/com/example/anweshmishra/kotlinsequentialbarsview/MainActivity.kt
+++ app/src/main/java/com/example/anweshmishra/kotlinsequentialbarsview/MainActivity.kt
@@ -3,13 +3,17 @@ package com.example.anweshmishra.kotlinsequentialbarsview
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import android.widget.Toast
 import com.example.sequentialbarsview.SequentialsBarView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        SequentialsBarView.create(this)
+        val view : SequentialsBarView = SequentialsBarView.create(this)
+        view.addOnMoveListener {
+            Toast.makeText(this, "$it bar is moved", Toast.LENGTH_SHORT).show()
+        }
         fullScreen()
     }
 }
